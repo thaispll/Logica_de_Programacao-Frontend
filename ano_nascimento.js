4b47b2ac9857f0b3b53bb32c80695c74f3cb5317
Thais De Paula Leal Cobucci <tcobucci@firjan.com.br>
2026-01-20 21:31:07 -0300

    primeiro commit do curso

---
+++ ano_nascimento.js
@@ -0,0 +1,10 @@
+let idade = 28;
+//criar variável ano atual
+let anoAtual = 2026;
+//cálculo 
+let anoNascimento = anoAtual - idade;
+
+//mostrar para o usuário
+console.log(`Idade informada: ${idade}`);
+console.log(`Ano atual: ${anoAtual}`);
+console.log(`Você nasceu em ${anoNascimento}`);
---
+++ input_output.js
@@ -0,0 +1,23 @@
+let nomeUsuario = "Thais";
+//alert: popup web
+console.log("Olá, "+nomeUsuario);
+
+const pi = 3.14; //
+console.log("Pi = "+pi);
+
+//template literals: resume a frase
+let idade = 28;
+let cidade = "Volta Redonda";
+console.log("Olá, meu nome é "+nomeUsuario + ", tenho " +idade+ " anos e moro em "+cidade);
+console.log(`Olá, meu nome é ${nomeUsuario}, tenho ${idade} anos e moro em ${cidade}`);
+//Comentário 1 linha
+/* Comentário
+com várias linhas */
+
+//var,let e const
+/* Variáveis são nomes simbólicos que apontam para
+espaços na memória do computador, usados para armazenar dados
+let: declaro variável limitando o escopo no bloco, instrução. Variável limitada
+var: Variável global, pode ser acessada de qualquer lugar do programa
+const: constante, imutável - algo fixo. Ex.: Pi
+*/
